648d486e682c7fbff87fd7bde1c97eff04ad6ed2
Nikolas Haug <nikohaug21@gmail.com>
2019-10-16 15:59:37 -0500

    add index routes

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,54 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
+// GET /register
+router.get('/register', (req, res, next) => {
+  res.send('GET /register');
+});
+
+// POST /register
+router.post('/register', (req, res, next) => {
+  res.send('POST /register');
+});
+
+// GET /login
+router.get('/login', (req, res, next) => {
+  res.send('GET /login');
+});
+
+// POST /login
+router.post('/login', (req, res, next) => {
+  res.send('POST /login');
+});
+
+// GET /profile
+router.get('/profile', (req, res, next) => {
+  res.send('GET /profile');
+});
+
+// PUT /profile/:user_id
+router.put('/profile/:user_id', (req, res, next) => {
+  res.send('GET /profile/:user_id');
+});
+
+// GET /forgot-password
+router.get('/forgot', (req, res, next) => {
+  res.send('GET /forgot');
+});
+
+// PUT /forgot
+router.put('/forgot', (req, res, next) => {
+  res.send('PUT /forgot');
+});
+
+// GET /reset/:token
+router.get('/reset/:token', (req, res, next) => {
+  res.send('GET /reset/:token');
+});
+
+// PUT /reset/:token
+router.put('/reset/:token', (req, res, next) => {
+  res.send('PUT /reset/:token');
+});
+
 module.exports = router;
