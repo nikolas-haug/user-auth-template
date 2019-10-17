ec93695c657ca0cb1a80a341131cc081cff54030
Nikolas Haug <nikohaug21@gmail.com>
2019-10-17 12:09:52 -0500

    connect to mongodb

--- app.js
+++ app.js
@@ -3,10 +3,29 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
+const User = require('./models/user');
+const mongoose = require('mongoose');
 
+// Require routes
 const indexRouter = require('./routes/index');
 
 const app = express();
+
+// Connect to the mongodb Database ======================
+mongoose.connect('mongodb://localhost:27017/auth-template', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.set('useCreateIndex', true);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('connected to mongodb database');
+});
+// ======================================================
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +37,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Configure Passport and Sessions ======================
+app.use(session({
+  secret: 'anythingyouwanthere',
+  resave: false,
+  saveUninitialized: true
+}));
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+// ======================================================
+
+// Mount routes
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
+const { postRegister } = require('../controllers/index');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -14,9 +15,7 @@ router.get('/register', (req, res, next) => {
 });
 
 // POST /register
-router.post('/register', (req, res, next) => {
-  res.send('POST /register');
-});
+router.post('/register', postRegister);
 
 // GET /login
 router.get('/login', (req, res, next) => {
