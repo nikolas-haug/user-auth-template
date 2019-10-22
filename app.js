7dbe3015f22025ce774b1ef92334d949f758592d
Nikolas Haug <nikohaug21@gmail.com>
2019-10-22 16:00:25 -0500

    update final error handler in app js

--- app.js
+++ app.js
@@ -78,16 +78,8 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  // res.locals.message = err.message;
-  // res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // // render the error page
-  // res.status(err.status || 500);
-  // res.render('error');
-
-  // Log the error in productions
+app.use(function(err, req, res, next) {  
+  // Log the error in development
   console.log(err);
   // Add session error then redirect
   req.session.error = err.message;
